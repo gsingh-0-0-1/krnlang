24ae5518501b1a5a22a2c0084aa527a4328983a3
Gurmehar Singh <gurmehar@gmail.com>
2024-01-02 00:31:18 -0800

    add name and email

--- static/js/menubar.js
+++ static/js/menubar.js
@@ -1,10 +1,15 @@
-var menubarreq = new XMLHttpRequest;
-menubarreq.open("GET", "/static/menubar.html")
-menubarreq.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var el = document.createElement('div')
-		el.innerHTML = this.responseText
-		document.body.appendChild(el)
+function requestTemplateEl(name) {
+	var req = new XMLHttpRequest;
+	req.open("GET", "/static/" + name + ".html")
+	req.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var el = document.createElement('div')
+			el.innerHTML = this.responseText
+			document.body.appendChild(el)
+		}
 	}
+	req.send()
 }
-menubarreq.send()
+
+requestTemplateEl("menubar")
+requestTemplateEl("credits")
